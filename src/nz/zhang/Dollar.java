8188683865710ec71bf3a538b5295929dff713cf
Edward Zhang <edward@zhang.nz>
2019-04-05 10:14:18 +1300

    Initial commit

---
+++ src/nz/zhang/Dollar.java
@@ -0,0 +1,29 @@
+package nz.zhang;
+
+public class Dollar {
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        int resultantAmount = this.amount * multiplier;
+        return new Dollar(resultantAmount);
+    }
+
+    public Dollar dividedBy(int denominator) {
+        int resultantAmount = this.amount / denominator;
+        return new Dollar(resultantAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "$" + this.amount;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Dollar && this.amount == ((Dollar) other).amount;
+    }
+}
